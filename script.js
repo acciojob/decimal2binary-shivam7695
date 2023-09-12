09ae71e89bd6fe52855eacc1de177c636af80be8
Shivam Jadhav <127479563+shivam7695@users.noreply.github.com>
2023-09-12 19:23:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+let binary = "";
+  let quotient = decimal;
+
+  while (quotient > 0) {
+    remainder = quotient % 2;
+    binary = remainder + binary;
+    quotient = Math.floor(quotient / 2);
+  }
+
+  return binary;
 }
 
-window.decimalToBinary = decimalToBinary;
+console.log(decimalToBinary(10)); // 1010
+console.log(decimalToBinary(33)); // 100001
